639d6d03ae9a444f21e2066aac12de9f78f58e93
keshew <97288599+keshew@users.noreply.github.com>
2022-09-07 12:52:16 +0500

    done

--- fire/Controllers/TaskViewController.swift
+++ fire/Controllers/TaskViewController.swift
@@ -31,12 +31,44 @@ class TaskViewController: UIViewController, UITableViewDataSource, UITableViewDe
         tasks.count
     }
     
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        ref.removeAllObservers()
+    }
+    
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            let task = tasks[indexPath.row]
+            task.ref?.removeValue()
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let cell = tableView.cellForRow(at: indexPath) else { return }
+        let task = tasks[indexPath.row]
+        let isCompleted = !task.completed
+        toggleCompletion(cell, isCompleted: isCompleted)
+        task.ref?.updateChildValues(["completed": isCompleted])
+    }
+    
+    func toggleCompletion(_ cell: UITableViewCell, isCompleted: Bool) {
+        cell.accessoryType = isCompleted ? .checkmark : .none
+    }
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let task = tasks[indexPath.row]
+        let taskTitle = task.title
+        let isCompleted = task.completed
         cell.backgroundColor = .clear
         cell.textLabel?.textColor = .white
-        let taskTitle = tasks[indexPath.row].title
         cell.textLabel?.text = taskTitle
+        toggleCompletion(cell, isCompleted: isCompleted)
 
         return cell
     }
